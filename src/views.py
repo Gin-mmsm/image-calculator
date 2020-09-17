5256bf35b3ebfe9dcdf0df123373a64344cd742d
m-star18 <ryuuchei@ezweb.ne.jp>
2020-09-17 10:36:29 +0900

    Add GET

--- src/views.py
+++ src/views.py
@@ -4,3 +4,9 @@ import werkzeug
 
 from src import app
 from src.calculator import ocr_image, txt_calculation
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'GET':
+        return render_template('layout.html')
