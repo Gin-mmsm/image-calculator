a890f10a169f11f747b13ec819e2fc896aa70fdc
Gin-mmsm <shimojimaginshi@gmail.com>
2020-09-18 01:49:11 +0900

    ファイル整理

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-app = Flask(__name__, static_folder='../assets')
+app = Flask(__name__, static_folder='static/')
 
 # limit upload file size : 10MB
 app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024
