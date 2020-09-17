0a1ec14ad19b32d71326732f02de5aba8b9e7414
m-star18 <ryuuchei@ezweb.ne.jp>
2020-09-17 10:36:56 +0900

    Add setup file

--- src/views.py
+++ src/views.py
@@ -10,3 +10,11 @@ from src.calculator import ocr_image, txt_calculation
 def upload_file():
     if request.method == 'GET':
         return render_template('layout.html')
+
+    if request.method == 'POST':
+        # ファイルが設定されているか
+        if 'uploadFile' not in request.files:
+            make_response(jsonify({'result': 'upladFile is required.'}))
+        file = request.files['uploadFile']
+        if '' == file.filename:
+            make_response(jsonify({'result': 'filename must not empty.'}))
